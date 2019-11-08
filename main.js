37591b18cc2d380369473070100409cc31caa5b5
Abhishek <abhishek154@live.com>
2019-11-08 16:22:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
 	socket.emit('chat message', msg);
+	socket.broadcast.emit('chat message', msg);  
   });
 });
 
