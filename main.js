ff53500588d163b4c235726f2d356e5962cca9c1
Abhishek <abhishek154@live.com>
2019-12-31 00:13:34 +0530

    Fixed Persistence

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var fs = require("fs");
+var moment = require('moment');
+var express = require('express');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.use(express.static('public'))
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-	socket.emit('chat message', msg);
-	socket.broadcast.emit('chat message', msg);  
+	var msgg = moment() + ' : ' + msg + '\n';
+	console.log('Message: ' + msgg);
+	socket.emit('chat message', msgg);
+	socket.broadcast.emit('chat message', msgg);
+	fs.appendFile('./public/text.txt', msgg, function (err) {
+		  if (err) throw err;
+		  console.log('Saved!');
+	});
   });
 });
 
